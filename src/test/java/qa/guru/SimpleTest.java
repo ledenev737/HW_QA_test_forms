1bab24fe77df63dfbc034ddfcc593057d8c2c48c
ledenev737 <ledenev737@gmail.com>
2022-04-08 15:43:01 +0200

    Added 2 more tests and BeforeAll/AfterAll

--- src/test/java/qa/guru/SimpleTest.java
+++ src/test/java/qa/guru/SimpleTest.java
@@ -2,30 +2,53 @@ package qa.guru;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class SimpleTest {
 
+    @BeforeAll
+    static void initDB() {
+        System.out.println("### @BeforeAll");
+    }
+
     @BeforeEach
     void openYaPage() {
+        System.out.println("###     @BeforeEach");
         Selenide.open("https://ya.ru");
     }
 
     @AfterEach
     void close() {
+        System.out.println("###     @AfterEach");
         WebDriverRunner.closeWindow();
+    }
+
+    @AfterAll
+    static void cleanDB() {
+        System.out.println("### @AfterAll");
     }
 
     @Test
     void assertTest() {
+        System.out.println("###         @Test 0");
+        //...
+    }
+
+    @Test
+    void assertTest1() {
+        System.out.println("###         @Test 1");
         //...
     }
 
     @Test
     void assertTest2() {
+        System.out.println("###         @Test 2");
+        //...
+    }
+
+    @Test
+    void assertTest3() {
+        System.out.println("###         @Test 3");
         //...
     }
 }
